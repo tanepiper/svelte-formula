33c4e964968d5eef6a5b4874c2de2143652b59fb
Tane Piper <me@tane.dev>
2021-02-17 15:10:46 +0100

    Clean up touch handlers

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,6 @@
 const nxPreset = require('@nrwl/jest/preset');
 
-module.exports = { ...nxPreset };
+module.exports = {
+  ...nxPreset,
+  coverageReporters: ['html', 'lcov', 'text'],
+};
--- packages/svelte/formula/src/lib/touch.ts
+++ packages/svelte/formula/src/lib/touch.ts
@@ -2,27 +2,52 @@ import { FormEl } from '../types/forms';
 import { FormulaStores } from '../types/formula';
 
 /**
- * Create a handler for adding and removing focus events on elements
- * @param name
- * @param elements
- * @param stores
+ * Creates the handler for a group of elements for the touch event on the group name, once an
+ * element in the group has been touched, all element focus handlers will be removed
+ *
+ * @private
+ *
+ * @param name The name of the group to create the touch handlers for
+ * @param elements The elements that belong to the named group
+ * @param stores The stores for the form instance
+ *
+ * @returns Function that when called will remove all focus handlers from the elements, if not removed by user action
  */
 export function createTouchHandlers(name: string, elements: FormEl[], stores: FormulaStores) {
+  /**
+   * Internal map of element focus handlers
+   */
   const elMap = new Map<FormEl, (event: Event) => void>();
+
+  /**
+   * Method to call when handlers should be destroyed, clean up handlers then remove the handlers
+   */
+  const destroy = () => {
+    for (let [el, handler] of elMap) {
+      el.removeEventListener('focus', handler);
+    }
+    elMap.clear();
+  };
+
+  // Set the current touched state to false
   stores.touched.update((state) => ({ ...state, [name]: false }));
 
-  function updateTouched() {
+  /**
+   * Creates the instance of the handler for each element
+   */
+  function createElementHandler() {
     return () => {
       stores.touched.update((state) => ({ ...state, [name]: true }));
-      [...elMap].forEach(([el, handler]) => el.removeEventListener('focus', handler));
+      destroy();
     };
   }
 
-  elements.forEach((el) => {
-    const handler = updateTouched();
+  for (let i = 0; i < elements.length; i++) {
+    const el = elements[i];
+    const handler = createElementHandler();
     el.addEventListener('focus', handler);
     elMap.set(el, handler);
-  });
+  }
 
-  return () => [...elMap].forEach(([el, handler]) => el.removeEventListener('focus', handler));
+  return destroy;
 }
