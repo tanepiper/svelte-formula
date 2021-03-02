1b7ee268065844a9ab2f777e1cde731fe31b05d2
Tane Piper <tane-piper@idexx.com>
2021-03-02 23:22:03 +0100

    Remove unneeded subscriptions and correctly support group IDs

--- packages/svelte/formula/src/lib/group/group.ts
+++ packages/svelte/formula/src/lib/group/group.ts
@@ -2,6 +2,8 @@ import { Beaker, BeakerStores, Formula, FormulaOptions } from '../../types';
 import { createGroupStores } from '../shared/stores';
 import { createForm } from '../form/form';
 
+let groupCounter = 0;
+
 /**
  * Creates a group, which is really just a collection of forms for row data
  * @param options
@@ -12,55 +14,24 @@ export function createGroup<T extends Record<string, unknown | unknown[]>>(
   beakerStores: Map<string, BeakerStores<T>>,
 ): Beaker<T> {
   const stores = createGroupStores<T>(options);
-  let groupId;
+  let groupName;
   let groupParentNode: HTMLElement;
   let globalObserver: MutationObserver;
 
   const formSet = new Set<Formula<T>>();
   const instanceSet = new Set<{ destroy: () => void }>();
-  const subscriptions = new Map<string, (() => {})[]>();
 
   function destroyGroup() {
-    [...subscriptions].forEach(([key, subs]) => {
-      if (key === 'isFormValid' || key === 'isFormReady') {
-        stores[key].set(false);
-      } else {
-        stores[key].set([]);
-      }
-      subs.forEach((sub) => sub());
-    });
-    subscriptions.clear();
     instanceSet.forEach((instance) => instance.destroy());
     formSet.clear();
   }
 
   function groupHasChanged(rows: HTMLElement[]) {
     rows.forEach((row, i) => {
-      const form = createForm<T>(options);
+      const form = createForm<T>(options, undefined, groupName);
       const instance = form.form(row as HTMLElement, true);
       formSet.add(form);
       instanceSet.add(instance);
-
-      Object.entries(form.stores).forEach(([key, store]) => {
-        const unsub = store.subscribe((value) => {
-          stores[key].update((state) => {
-            if (Array.isArray(state)) {
-              state.splice(i, 1, value);
-            } else {
-              state = value;
-            }
-            return state;
-          });
-        });
-
-        if (!subscriptions.has(key)) {
-          subscriptions.set(key, [unsub]);
-        } else {
-          const subs = subscriptions.get(key);
-          subs.push(unsub);
-          subscriptions.set(key, subs);
-        }
-      });
     });
     stores.isFormReady.set(true);
   }
@@ -83,9 +54,12 @@ export function createGroup<T extends Record<string, unknown | unknown[]>>(
 
   return {
     group: (node: HTMLElement) => {
-      groupId = node.id;
-      if (groupId) {
-        beakerStores.set(groupId, stores);
+      if (node.id) {
+        groupName = node.id;
+        beakerStores.set(groupName, stores);
+      } else {
+        groupName = `beaker-group-${groupCounter++}`;
+        node.id = groupName;
       }
       groupParentNode = node;
 
@@ -95,8 +69,8 @@ export function createGroup<T extends Record<string, unknown | unknown[]>>(
 
       return {
         destroy: () => {
-          if (groupId) {
-            beakerStores.delete(groupId);
+          if (groupName) {
+            beakerStores.delete(groupName);
           }
           destroyGroup();
           globalObserver.disconnect();
@@ -110,8 +84,8 @@ export function createGroup<T extends Record<string, unknown | unknown[]>>(
       [...formSet].forEach((form) => form.resetForm());
     },
     destroy: () => {
-      if (groupId) {
-        beakerStores.delete(groupId);
+      if (groupName) {
+        beakerStores.delete(groupName);
       }
       destroyGroup();
       globalObserver.disconnect();
