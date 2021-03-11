f21995d43064e8a3950931265ea7a8a673efbe68
Tane Piper <tane-piper@idexx.com>
2021-03-11 17:23:02 +0100

    Don't call form validation method if novalidate set

--- apps/formula-app/src/pages/WeePage.svelte
+++ apps/formula-app/src/pages/WeePage.svelte
@@ -1,12 +1,26 @@
 <script>
-  import { formula } from "../../../../dist/packages/svelte/formula";
-  const { form, formValues } = formula();
+  import { formula } from '../../../../dist/packages/svelte/formula'
+  const { form, submitValues, isFormValid, validity } = formula();
+
+  let formSubmitted = false;
+  function submit() {
+    formSubmitted = true;
+    if ($isFormValid) {
+      alert('Passed validation')
+    } else {
+      console.log($isFormValid, $submitValues);
+    }
+  }
 </script>
 
-<form use:form>
-  <input type="text" name="firstName" required/>
-  <input type="text" name="lastName" />
-  <input type="number" name="age" />
+<form on:submit|preventDefault={submit} novalidate use:form>
+  <fieldset>
+    <input type="text" required name="zzz" />
+    <span class="validation" class:error={formSubmitted && $validity?.zzz?.invalid}>{$validity?.zzz?.message}</span>
+  </fieldset>
+  <button disabled="{formSubmitted && !$isFormValid}" type="submit">Submit</button>
 </form>
 
-<pre>{JSON.stringify($formValues, null, 2)}</pre>
+<style>
+  .validation:not(.error) { display: none }
+</style>
